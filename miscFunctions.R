1aa696ceb1637b5904a632d49e3125e2af52d346
Simon Forsberg <sf9@princeton.edu>
2021-04-21 14:15:17 +0200

    qqplot
    A handy qqplot function

--- miscFunctions.R
+++ miscFunctions.R
@@ -1277,4 +1277,26 @@ clump.markers <- function (assoc, snp.cor, chr = 1, bp.dist = 250000, p1 = 1e-04
   clumps
 }
 
-
+#Taken from here https://slowkow.com/notes/ggplot2-qqplot/
+gg_qqplot <- function(ps, ci = 0.95) {
+  n  <- length(ps)
+  df <- data.frame(
+    observed = -log10(sort(ps)),
+    expected = -log10(ppoints(n)),
+    clower   = -log10(qbeta(p = (1 - ci) / 2, shape1 = 1:n, shape2 = n:1)),
+    cupper   = -log10(qbeta(p = (1 + ci) / 2, shape1 = 1:n, shape2 = n:1))
+  )
+  log10Pe <- expression(paste("Expected -log"[10], plain(P)))
+  log10Po <- expression(paste("Observed -log"[10], plain(P)))
+  ggplot(df) +
+    geom_ribbon(
+      mapping = aes(x = expected, ymin = clower, ymax = cupper),
+      alpha = 0.1
+    ) +
+    geom_point(aes(expected, observed), shape = 1, size = 3) +
+    geom_abline(intercept = 0, slope = 1, alpha = 0.5) +
+    # geom_line(aes(expected, cupper), linetype = 2, size = 0.5) +
+    # geom_line(aes(expected, clower), linetype = 2, size = 0.5) +
+    xlab(log10Pe) +
+    ylab(log10Po)
+}
